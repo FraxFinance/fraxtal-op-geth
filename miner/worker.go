a2f963a3f72977c60ee155adb8aaf36743bb1348
Alessandro Tagliapietra <0xalex88@proton.me>
2024-12-30 16:18:31 -0800

    Add frxUSD/sfrxUSD granite fork code change

--- miner/worker.go
+++ miner/worker.go
@@ -209,6 +209,7 @@ func (miner *Miner) generateWork(ctx context.Context, genParam *generateParams, 
 	}
 
 	misc.EnsureCreate2Deployer(miner.chainConfig, work.header.Time, work.state)
+	misc.EnsureFrxUSD(miner.chainConfig, work.header.Time, work.state)
 
 	for _, tx := range genParam.txs {
 		from, _ := types.Sender(work.signer, tx)
