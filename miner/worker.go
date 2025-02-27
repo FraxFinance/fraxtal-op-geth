dae3107744e0894701bd6eb2430da01eb005780f
Alessandro Tagliapietra <0xalex88@proton.me>
2025-02-26 22:32:08 -0800

    Add frax tokens holocene fork code change

--- miner/worker.go
+++ miner/worker.go
@@ -147,6 +147,7 @@ func (miner *Miner) generateWork(params *generateParams, witness bool) *newPaylo
 
 	misc.EnsureCreate2Deployer(miner.chainConfig, work.header.Time, work.state)
 	misc.EnsureFrxUSD(miner.chainConfig, work.header.Time, work.state)
+	misc.EnsureFraxTokensProxies(miner.chainConfig, work.header.Time, work.state)
 
 	for _, tx := range params.txs {
 		from, _ := types.Sender(work.signer, tx)
